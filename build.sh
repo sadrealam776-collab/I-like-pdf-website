b14d81c3365f279c88604e9f3169eb1e795404e7
sadrealam776-collab <sadrealam776@gmail.com>
2026-09-16 13:23:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
+# Upgrade pip, setuptools, and wheel to support modern pre-built binary wheels
+python -m pip install --upgrade pip setuptools wheel
+
 # Install system-level Tesseract-OCR for Linux
-apt-get update
-apt-get install -y tesseract-ocr
+apt-get update && apt-get install -y tesseract-ocr
 
 # Install Python dependencies
 pip install -r requirements.txt
